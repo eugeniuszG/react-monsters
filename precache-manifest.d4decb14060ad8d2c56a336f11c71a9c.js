ab45366daccdf4b298834ef80dccd37088808942
eugeneboroda <41344500+eugeneboroda@users.noreply.github.com>
2020-05-07 20:12:59 +0200

    Updates

---
+++ precache-manifest.d4decb14060ad8d2c56a336f11c71a9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bd36e32a3f1f32a33de8c3ea14ec7d9",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "b7ef9632d9222daaaffb",
+    "url": "/react-monsters/static/css/main.f5a4e47e.chunk.css"
+  },
+  {
+    "revision": "2b7d9edd948003b96165",
+    "url": "/react-monsters/static/js/2.b6796cae.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-monsters/static/js/2.b6796cae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7ef9632d9222daaaffb",
+    "url": "/react-monsters/static/js/main.118689bc.chunk.js"
+  },
+  {
+    "revision": "b86f61f56ca252e9b7fe",
+    "url": "/react-monsters/static/js/runtime-main.ee168a32.js"
+  }
+]);
